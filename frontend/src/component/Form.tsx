5ecd31bbf52df0406699356b5b12ffe2f495009f
Irvi Aini <irvi.fa@gmail.com>
2023-07-18 15:37:04 +0800

    Use state.

--- frontend/src/component/Form.tsx
+++ frontend/src/component/Form.tsx
@@ -1,52 +1,115 @@
 import "react-app-polyfill/ie11";
-import * as React from "react";
+import React, { Component } from "react";
 import * as ReactDOM from "react-dom";
-import { Formik, Field, Form, FormikHelpers } from "formik";
+import Button from "@mui/material/Button";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import "./Form.css";
 
-interface Values {
-  firstName: string;
-  lastName: string;
-  email: string;
+interface Props {}
+interface State {
+  url: string;
+  result: string;
+  loading: boolean;
 }
+export class Form extends Component<Props, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      url: "test  ",
+      result: "",
+      loading: false,
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.isLoading = this.isLoading.bind(this);
+  }
 
-const App = () => {
-  return (
-    <div>
-      <h1>Signup</h1>
-      <Formik
-        initialValues={{
-          firstName: "",
-          lastName: "",
-          email: "",
-        }}
-        onSubmit={(
-          values: Values,
-          { setSubmitting }: FormikHelpers<Values>,
-        ) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 500);
+  handleSubmit(e: any) {
+    e.preventDefault();
+    // 👇 call backend endpoint using fetch API
+    this.setState({
+      loading: true,
+    });
+    fetch("http://127.0.0.1:5000/api/transcribe", {
+      body: JSON.stringify({ url: this.state.url }),
+      method: "post",
+      headers: {
+        "content-type": "application/json",
+      },
+    }).then(async (result) => {
+      // 👇 modify the state to show the result
+      const resp = await result.json();
+      this.setState({
+        result: resp.transcript,
+        loading: false,
+      });
+    });
+  }
+
+  handleChange(event: any) {
+    this.setState({
+      url: event.target.value,
+    });
+  }
+
+  isLoading() {
+    return this.state.loading;
+  }
+
+  render() {
+    if (this.isLoading()) {
+      return (
+        <Box>
+          <div className="spinner"></div>
+        </Box>
+      );
+    }
+    return (
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
         }}
       >
-        <Form>
-          <label htmlFor="firstName">First Name</label>
-          <Field id="firstName" name="firstName" placeholder="John" />
-
-          <label htmlFor="lastName">Last Name</label>
-          <Field id="lastName" name="lastName" placeholder="Doe" />
-
-          <label htmlFor="email">Email</label>
-          <Field
-            id="email"
-            name="email"
-            placeholder="john@acme.com"
-            type="email"
+        <Typography component="h1" variant="h5">
+          Transcribe & Summarize
+        </Typography>
+        <Box
+          component="form"
+          onSubmit={this.handleSubmit}
+          noValidate
+          sx={{ mt: 1 }}
+        >
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="url"
+            label="YouTube URL"
+            name="url"
+            autoComplete="url"
+            autoFocus
+            onChange={this.handleChange}
           />
-
-          <button type="submit">Submit</button>
-        </Form>
-      </Formik>
-    </div>
-  );
-};
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Transcribe
+          </Button>
+        </Box>
+        <Typography component="h5" variant="h5">
+          Transcript
+        </Typography>
+        <Box sx={{ mt: 3, mb: 2, width: "75%" }}>{this.state.result}</Box>
+      </Box>
+    );
+  }
+}
